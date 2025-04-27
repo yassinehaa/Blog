d368934a24d4468d064e072b3ffdd60aba37dd16
simodev744 <ammar.mohamed744@gmail.com>
2025-04-27 17:18:35 +0200

    creation du service category

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface Category {
+  id: number;
+  name: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  private apiUrl = 'http://localhost:3000/categories';
+
+  constructor(private http: HttpClient) {}
+
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.apiUrl);
+  }
+}
